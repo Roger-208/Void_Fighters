4643479e2e04e3f05d0243ab0f05405c313cdf62
Nil <nilonly46@gmail.com>
2026-05-05 08:56:04 +0200

    Canvis start.sh

--- start.sh
+++ start.sh
@@ -4,9 +4,9 @@ cd ./projecte_docker
 
 echo "Iniciant servidor Void Fighters"
 
+{
 docker compose up --build
-
-echo "Iniciat servidor Void Fighters"
-
-echo "Pagina Web disponible en http://IP:8080"
-echo "Servidor del joc disponible en la IP de l'ordinador"
+} || {
+echo -e "\033[1mLa posada en funcionament ha fallat"
+exit 1
+}
